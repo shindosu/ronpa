ac2808ae5d5603a132ee72e27cc04f83f8e4e81d
bogdanmatei <matei90bogdan@yahoo.com>
2019-11-19 14:46:35 +0900

    implemented dashboard view if logged in or not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    if params[:id].present?
+      @user = User.find(params[:id])
+    else
+      @user = current_user
+    end
   end
 end

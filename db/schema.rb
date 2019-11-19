a941362f9f19a901ab972d52eafbfa21b3a04bcb
bogdanmatei <matei90bogdan@yahoo.com>
2019-11-19 13:40:45 +0900

    basic landing page

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_11_19_014452) do
-
+ActiveRecord::Schema.define(version: 2019_11_19_021900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "debates", force: :cascade do |t|
+    t.bigint "topic_id"
+    t.string "phase"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_debates_on_topic_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "proposition"
+    t.string "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+    t.string "category"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,14 +51,6 @@ ActiveRecord::Schema.define(version: 2019_11_19_014452) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  
-  create_table "topics", force: :cascade do |t|
-    t.string "proposition"
-    t.string "level"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "description"
-    t.string "category"
-  end
 
+  add_foreign_key "debates", "topics"
 end

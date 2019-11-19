c6c549af1bbbde209432b240d3c328bffa46e90c
Noah Thorne <noahlthorne@gmail.com>
2019-11-19 17:59:01 +0900

    added a bit to the uses controller but didnt finish

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -2,7 +2,7 @@ class ParticipantsController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @debate = Debate.waiting_start.find_by(topic_id: @topic.id)
-    if @debate
+    if @debate && !current_user.in_debate?
       @participant = Participant.new(debate: @debate, user: current_user)
       if @debate.participants.count == 1
         @participant.affirmative!
--- db/migrate/20191119021834_create_debates.rb
+++ db/migrate/20191119021834_create_debates.rb
@@ -2,7 +2,7 @@ class CreateDebates < ActiveRecord::Migration[5.2]
   def change
     create_table :debates do |t|
       t.references :topic, foreign_key: true
-      t.string :phase
+      t.integer :phase
 
       t.timestamps
     end

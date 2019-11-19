cd6cdc913dc565c81fad80497436c12b69aa7498
Noah Thorne <noahlthorne@gmail.com>
2019-11-19 14:10:02 +0900

    created debates controller with show and create actions

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -2,4 +2,16 @@ class Debate < ApplicationRecord
   belongs_to :topic
   has_many :uses
   has_many :participants
+
+  enum phase: [
+    :waiting_start, :affirmative_speech,
+    :negative_cross_examination_preparation,
+    :negative_cross_examination,
+    :negative_speech,
+    :positive_cross_examination_preparation,
+    :positive_cross_examination,
+    :negative_closing_statement,
+    :positive_closing_statement,
+    :waiting_end
+  ]
 end

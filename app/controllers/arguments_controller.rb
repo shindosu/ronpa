a0eca7ea6fa3be495aa9b89467d57f6154e1be48
Edward Blundell <ewblundell@gmail.com>
2019-11-20 11:23:37 +0900

    created all argument forms partial

--- app/controllers/arguments_controller.rb
+++ app/controllers/arguments_controller.rb
@@ -1,8 +1,10 @@
 class ArgumentsController < ApplicationController
   def create
-    @topic = Topic.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
     @argument = Argument.new(argument_params)
-    if @argumuent.save
+    @argument.user = current_user
+    @argument.topic = @topic
+    if @argument.save
       redirect_to topic_path(@topic)
     else
       render 'topics/show'

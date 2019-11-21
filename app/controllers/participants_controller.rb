248706df7604b0b355c011faebb4ea0819518dcf
Noah Thorne <noahlthorne@gmail.com>
2019-11-21 11:23:36 +0900

    created view page and added pundit authorizations

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,15 +1,17 @@
 class ParticipantsController < ApplicationController
   def create
-    @topic = Topic.find(params[:topic_id])
-    @debate = Debate.waiting_start.find_by(topic_id: @topic.id)
+    # @topic = Topic.find(params[:topic_id])
+    # @debate = Debate.waiting_start.find_by(topic_id: @topic.id)
     if @debate && !current_user.in_debate?
       @participant = Participant.new(debate: @debate, user: current_user)
+      authorize @participant
       if @debate.participants.count == 1
         @participant.affirmative!
       else
         @participant.negative!
       end
     else
+      authorize @participant
       render "topics/show"
     end
   end

e89c89858fc70c06b68eb7144c51b7a8573ec44d
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-11-21 16:55:42 +0900

    edited show and fixed uses controller

--- app/controllers/uses_controller.rb
+++ app/controllers/uses_controller.rb
@@ -1,7 +1,8 @@
 class UsesController < ApplicationController
   def create
     Participant.affirmative.first.user.arguments.where(topic_id: debate.topic_id).affirmative
-
+  end
+  
   def update
   end
 end

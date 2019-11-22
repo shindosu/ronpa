c71ba27cea66f172e9c208b19b0dd0bcca2bb7f9
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-11-22 12:55:49 +0900

    case statement done for moderator

--- app/javascript/components/timer.js
+++ app/javascript/components/timer.js
@@ -4,11 +4,42 @@ let timer = new Timer();
 let phase_status = document.querySelector(".phase-status")
 
 switch (phase_status) {
+
   case "waiting start":
       timer.start({countdown: true, startValues: {seconds: 30}});
   break;
   case "count in":
       timer.start({countdown: true, startValues: {seconds: 10}});
+  break;
+  case "affirmative speech":
+      timer.start({countdown: true, startValues: {seconds: 480}});
+  break;
+  case "negative cross examination preparation":
+      timer.start({countdown: true, startValues: {seconds: 10}});
+  break;
+  case "negative cross examination":
+      timer.start({countdown: true, startValues: {seconds: 480}});
+  break;
+  case "negative_speech":
+      timer.start({countdown: true, startValues: {seconds: 480}});
+  break;
+  case "affirmative cross examination preparation":
+      timer.start({countdown: true, startValues: {seconds: 10}});
+  break;
+  case "affirmative cross examination":
+      timer.start({countdown: true, startValues: {seconds: 480}});
+  break;
+  case "negative closing statement":
+      timer.start({countdown: true, startValues: {seconds: 480}});
+  break;
+  case "affirmative closing statement":
+      timer.start({countdown: true, startValues: {seconds: 480}});
+  break;
+  case "waiting end":
+      timer.start({countdown: true, startValues: {seconds: 30}});
+  break;
+  case "finished":
+      timer.start({countdown: true, startValues: {seconds: 0}});
   break;
 }
 
@@ -18,10 +49,6 @@ const button = document.querySelector(".button")
 timer.addEventListener('secondsUpdated', (event) => {
   values.innerHTML = timer.getTimeValues().toString()
 });
-
-timer.addEventListener('targetAchieved', (event) => {
-  alert("Phase end")
-})
 
 button.addEventListener("click", (event) => {
   timer.stop();

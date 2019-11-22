880f8dbfdcd2077e004e247a1a80803a7b37c352
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-11-22 11:32:01 +0900

    working on timer

---
+++ app/javascript/components/timer.js
@@ -0,0 +1,4 @@
+import Timer from 'easytimer.js';
+var timer = new Timer();
+
+console.log(timer)
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import { toggleMenu } from 'components/sidebar';
 import 'topic-show/show';
+import 'components/timer';
 
 toggleMenu();
 generateTextAnimated();

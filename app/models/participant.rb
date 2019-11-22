1bf6ddf85d7d1a8d46f7b76737c9d277e330213d
Edward Blundell <ewblundell@gmail.com>
2019-11-22 15:32:39 +0900

    moderator can set winner and loser

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,6 +3,7 @@ class Participant < ApplicationRecord
   belongs_to :debate
 
   enum role: [:affirmative, :negative, :moderator]
+  enum result: { pending: 0, winner: 1, loser: 2 }
 
   validates :user, :debate, :role, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_030521) do
+ActiveRecord::Schema.define(version: 2019_11_22_055224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_030521) do
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "result", default: 0
     t.index ["debate_id"], name: "index_participants_on_debate_id"
     t.index ["user_id"], name: "index_participants_on_user_id"
   end

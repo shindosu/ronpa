158ca0ea9eb0122fb056f411a9c1d07cb4c7a343
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-11-22 11:34:07 +0900

    working

--- app/javascript/topic-show/show.js
+++ app/javascript/topic-show/show.js
@@ -1,8 +1,6 @@
 const icon = document.querySelector('.topic-show-icon');
 const link = document.querySelector('.div-link');
-const current_class = document.getElementsByTagName("I")[0].className
-
-console.log(current_class)
+const current_class = document.getElementsByTagName("I")[0].className;
 
 link.addEventListener("mouseenter", (event) => {
   icon.className = "fas fa-arrow-left topic-show-icon fa-3x"

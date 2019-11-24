8546edfa7eb3a8332e2cfe31c264ed2a1da5991a
bogdanmatei <matei90bogdan@yahoo.com>
2019-11-24 20:17:09 +0900

    created flipable card

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,10 @@ import "bootstrap";
 import 'components/timer';
 import 'packs/topic-show';
 import { initTimer } from 'components/timer';
-import { flip } from 'components/cardflip';
+// import { flip } from 'components/cardflip';
+import { initFlip } from 'components/flip';
 
 initTimer();
-flip();
+// flip();
+initFlip();
+

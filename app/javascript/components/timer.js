c4d0e85c87d219669d92be39ff885159e4c5f7cf
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-11-25 18:27:39 +0900

    timer only shows on refresh

--- app/javascript/components/timer.js
+++ app/javascript/components/timer.js
@@ -1,67 +1,57 @@
 import { Timer } from 'easytimer.js';
 
-const setTimer = (phase_status, timer) => {
-  switch (phase_status) {
-  case "Affirmative opening arguments will start in:":
-      timer.start({countdown: true, startValues: {seconds: 21}});
-  break;
-  case "Affirmative opening arguments":
-      timer.start({countdown: true, startValues: {seconds: 121}});
-  break;
-  case "Negative cross examination preparation":
-      timer.start({countdown: true, startValues: {seconds: 31}});
-  break;
-  case "Negative cross examination":
-      timer.start({countdown: true, startValues: {seconds: 121}});
-  break;
-  case "Negative opening argument preparation":
-      timer.start({countdown: true, startValues: {seconds: 21}});
-  break;
-  case "Negative opening arguments":
-      timer.start({countdown: true, startValues: {seconds: 121}});
-  break;
-  case "Affirmative cross examination preparation":
-      timer.start({countdown: true, startValues: {seconds: 31}});
-  break;
-  case "Affirmative cross examination":
-      timer.start({countdown: true, startValues: {seconds: 121}});
-  break;
-  case "Affirmative closing statement prepatation":
-      timer.start({countdown: true, startValues: {seconds: 11}});
-  break;
-  case "Affirmative closing statements":
-      timer.start({countdown: true, startValues: {seconds: 61}});
-  break;
-  case "Negative closing statement preparation":
-      timer.start({countdown: true, startValues: {seconds: 11}});
-  break;
-  case "Negative closing statements":
-      timer.start({countdown: true, startValues: {seconds: 61}});
-  break;
-  case "Moderator giving final score...":
-      timer.start({countdown: true, startValues: {seconds: 31}});
-  }
-}
-
-const initTimer = () => {
-  const phaseNode = document.querySelector(".text-phase")
-  if (phaseNode) {
     const timer = new Timer();
     let values = document.querySelector(".values");
     let button = document.querySelector(".next-phase");
-    let phase_status = phaseNode.innerText;
+    let phase_status = document.querySelector(".phase-text h3").innerText
 
-    setTimer(phase_status, timer);
+        switch (phase_status) {
+            case "Affirmative opening arguments will start in:":
+                console.log(phase_status == "Affirmative opening arguments will start in:")
+                timer.start({countdown: true, startValues: {seconds: 21}});
+            break;
+            case "Affirmative opening arguments":
+                timer.start({countdown: true, startValues: {seconds: 121}});
+            break;
+            case "Negative cross examination preparation":
+                timer.start({countdown: true, startValues: {seconds: 31}});
+            break;
+            case "Negative cross examination":
+                timer.start({countdown: true, startValues: {seconds: 121}});
+            break;
+            case "Negative opening argument preparation":
+                timer.start({countdown: true, startValues: {seconds: 21}});
+            break;
+            case "Negative opening arguments":
+                timer.start({countdown: true, startValues: {seconds: 121}});
+            break;
+            case "Affirmative cross examination preparation":
+                timer.start({countdown: true, startValues: {seconds: 31}});
+            break;
+            case "Affirmative cross examination":
+                timer.start({countdown: true, startValues: {seconds: 121}});
+            break;
+            case "Affirmative closing statement prepatation":
+                timer.start({countdown: true, startValues: {seconds: 11}});
+            break;
+            case "Affirmative closing statements":
+                timer.start({countdown: true, startValues: {seconds: 61}});
+            break;
+            case "Negative closing statement preparation":
+                timer.start({countdown: true, startValues: {seconds: 11}});
+            break;
+            case "Negative closing statements":
+                timer.start({countdown: true, startValues: {seconds: 61}});
+            break;
+            case "Moderator giving final score...":
+                timer.start({countdown: true, startValues: {seconds: 31}});
+            }
+        
+            timer.addEventListener('secondsUpdated', (event) => {
+              values.innerHTML = timer.getTimeValues().toString().split("").slice(3,8).join("")
+            });
+        
+            timer.addEventListener('targetAchieved', (event) => {
+              button.click();
+            });
 
-
-    timer.addEventListener('secondsUpdated', (event) => {
-      values.innerHTML = timer.getTimeValues().toString().split("").slice(3,8).join("")
-    });
-
-    timer.addEventListener('targetAchieved', (event) => {
-      button.click();
-    });
-  }
-}
-
-export { initTimer };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,10 @@
 import "bootstrap";
+import 'components/topic-show';
 import 'components/timer';
-import { initTimer } from 'components/timer';
 import { initFlip } from 'components/flip';
 import { initIconHover } from 'components/topic-show';
 import { submitForm } from 'components/argument-form';
 
-initTimer();
 initFlip();
 initIconHover();
 submitForm();

670a40f8851f7d2280f09aa31c6f7327eca9b4ec
bogdanmatei <matei90bogdan@yahoo.com>
2019-11-25 18:28:28 +0900

    logo animation done

---
+++ app/javascript/components/logo.js
@@ -0,0 +1,59 @@
+// Wrap every letter in a span
+// const textWrapper = document.querySelector('.ml11 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+
+const initLetter = () => {
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+}
+
+const initPop = () => {
+anime({
+  targets: '.el',
+  translateY: function(el) {
+    return el.getAttribute('data-x');
+  },
+  translateX: function(el, i) {
+    return 20 + (-20 * i);
+  },
+  scale: function(el, i, l) {
+    return (l / 3) ;
+  },
+  rotate: function() { return anime.random(0, 0); },
+  borderRadius: function() { return ['50%', anime.random(10, 35) + '%']; },
+  duration: function() { return anime.random(1200, 1800); },
+  delay: function() { return anime.random(0, 300); },
+  direction: 'alternate',
+  autoplay: true
+});
+}
+
+export { initPop };
+// export { initLetter };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,13 @@ import 'components/timer';
 import { initTimer } from 'components/timer';
 import { initFlip } from 'components/flip';
 import { initIconHover } from 'components/topic-show';
+import { initLetter } from 'components/logo';
+import { initPop } from 'components/logo';
 
+initPop();
+// initLetter();
 initTimer();
 initFlip();
 initIconHover();
+
 

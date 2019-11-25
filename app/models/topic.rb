8af1ae33a101102a54153a5981d3565d5e3aa65a
bogdanmatei <matei90bogdan@yahoo.com>
2019-11-25 09:07:59 +0900

    seeded

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ApplicationRecord
 CATEGORIES = %w{Apparel Entertainment Food General Politics}
 LEVELS = (1..5)
-  belongs_to :user  
+  belongs_to :user
   has_many :debates, dependent: :destroy
   has_many :arguments, dependent: :destroy
   validates :proposition, :level, :description, :category, presence: true

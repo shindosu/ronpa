50a18791dcb238d7de0b661b8883218e1c620aea
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-11-26 22:50:00 +0900

    added redis cloud url to redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-if ENV["REDISCLOUD_URL"]
-  $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+if ENV["'redis://rediscloud:HJ2V7621lEUMMrhNNbPVwmKVRvracr8R@redis-17478.c6.eu-west-1-1.ec2.cloud.redislabs.com:17478'"]
+  $redis = Redis.new(:url => ENV["'redis://rediscloud:HJ2V7621lEUMMrhNNbPVwmKVRvracr8R@redis-17478.c6.eu-west-1-1.ec2.cloud.redislabs.com:17478'"])
 end

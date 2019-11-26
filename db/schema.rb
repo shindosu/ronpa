e469cf27b854298869f672f3cdcba39e065a214a
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-11-26 15:09:51 +0900

    working on showing the numbers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_051258) do
+ActiveRecord::Schema.define(version: 2019_11_26_060414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2019_11_26_051258) do
     t.text "description"
     t.string "category"
     t.bigint "user_id"
+    t.integer "conference_number"
+    t.integer "access_code"
     t.index ["user_id"], name: "index_topics_on_user_id"
   end
 

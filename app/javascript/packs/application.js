38f87acc7cc8c2bdba18bef31eae41ff8cff8b3e
bogdanmatei <matei90bogdan@yahoo.com>
2019-11-26 09:13:41 +0900

    logo working now

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,20 +3,13 @@ import 'components/timer';
 import { initTimer } from 'components/timer';
 import { initFlip } from 'components/flip';
 import { initIconHover } from 'components/topic-show';
-
+import { submitForm } from 'components/argument-form';
 import { initLetter } from 'components/logo';
 import { initPop } from 'components/logo';
 
-<<<<<<< HEAD
+
 // initPop();
 initLetter();
-=======
-import { submitForm } from 'components/argument-form';
-
-
-initPop();
-// initLetter();
->>>>>>> master
 initTimer();
 initFlip();
 initIconHover();

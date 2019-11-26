7848249979f9a62303b46f5e283a9edac5d7813c
bogdanmatei <matei90bogdan@yahoo.com>
2019-11-26 09:42:04 +0900

    fixed js bug on landing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,6 @@ import 'components/topic-show';
 import {initTimer} from 'components/timer';
 import { initFlip } from 'components/flip';
 import { initIconHover } from 'components/topic-show';
-import { submitForm } from 'components/argument-form';
 import { initLetter } from 'components/logo';
 import { initPop } from 'components/logo';
 import { submitForm } from 'components/argument-form';

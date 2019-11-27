90b42b693ae162d93093d6c58a490d8db5a80239
Noah Thorne <noahlthorne@gmail.com>
2019-11-27 12:51:47 +0900

    dynamic timeline works

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -22,6 +22,41 @@ class Debate < ApplicationRecord
   } do
   end
 
+  PHASE_TITLES = {
+    waiting_start: "Finding participants...",
+    count_in: "count-in",
+    affirmative_speech: "Affirmative opening arguments",
+    negative_cross_examination_preparation: "Negative X exam prep",
+    negative_cross_examination: "Negative X exam",
+    negative_speech_preparation: "Negative opening argument prep",
+    negative_speech: "Negative opening arguments",
+    affirmative_cross_examination_preparation: "Affirmative X exam prep",
+    affirmative_cross_examination: "Affirmative X exam",
+    affirmative_closing_statement_preparation: "Affirmative closing prep",
+    affirmative_closing_statement: "Affirmative closing",
+    negative_closing_statement_preparation: "Negative closing prep",
+    negative_closing_statement: "Negative closing",
+    waiting_end: "final scoreing",
+    finished: "<h3>The winner is Someone</h3>"
+  }
+
+  AFFIRMATIVE_PHASES = [
+    :affirmative_speech,
+    :affirmative_cross_examination_preparation,
+    :affirmative_cross_examination,
+    :affirmative_closing_statement_preparation,
+    :affirmative_closing_statement
+  ]
+
+  NEGATIVE_PHASES = [
+      :negative_speech_preparation,
+      :negative_speech,
+      :negative_cross_examination_preparation,
+      :negative_cross_examination,
+      :negative_closing_statement_preparation,
+      :negative_closing_statement
+  ]
+
   scope :active, -> { where.not(phase: :finished) }
 
   def with_slot?(role)
@@ -29,24 +64,31 @@ class Debate < ApplicationRecord
   end
 
   def affirmative_turn?
-    [
-      :affirmative_speech,
-      :affirmative_cross_examination_preparation,
-      :affirmative_cross_examination,
-      :affirmative_closing_statement_preparation,
-      :affirmative_closing_statement
-    ].include?(phase.to_sym)
+    AFFIRMATIVE_PHASES.include?(phase.to_sym)
   end
 
   def negative_turn?
-    [
-      :negative_speech_preparation,
-      :negative_speech,
-      :negative_cross_examination_preparation,
-      :negative_cross_examination,
-      :negative_closing_statement_preparation,
-      :negative_closing_statement
-    ].include?(phase.to_sym)
+    NEGATIVE_PHASES.include?(phase.to_sym)
+  end
+
+  def phase_type
+    if affirmative_turn?
+      return "affirmative"
+    elsif negative_turn?
+      return "negative"
+    else
+      return "neutral"
+    end
+  end
+
+  def self.phase_type(phase)
+    if AFFIRMATIVE_PHASES.include?(phase)
+      return "affirmative"
+    elsif NEGATIVE_PHASES.include?(phase)
+      return "negative"
+    else
+      return "neutral"
+    end
   end
 
   def moderator

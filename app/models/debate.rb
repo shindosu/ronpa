c31cb0cdd10162a8e0aa3436a29d122925b43f1d
Noah Thorne <noahlthorne@gmail.com>
2019-11-28 11:20:28 +0900

    simplified debate flow

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -7,18 +7,20 @@ class Debate < ApplicationRecord
     waiting_start: 0,
     count_in: 1,
     affirmative_speech: 2,
-    negative_cross_examination_preparation: 3,
-    negative_cross_examination: 4,
-    negative_speech_preparation: 5,
-    negative_speech: 6,
-    affirmative_cross_examination_preparation: 7,
-    affirmative_cross_examination: 8,
-    affirmative_closing_statement_preparation: 9,
-    affirmative_closing_statement: 10,
-    negative_closing_statement_preparation: 11,
-    negative_closing_statement: 12,
-    waiting_end: 13,
-    finished: 14
+    negative_speech: 3,
+    # negative_cross_examination_preparation: 3,
+    affirmative_cross_examination: 4,
+    negative_cross_examination: 5,
+    # negative_speech_preparation: 5,
+    # negative_speech: 6,
+    # affirmative_cross_examination_preparation: 7,
+    # affirmative_cross_examination: 8,
+    # affirmative_closing_statement_preparation: 9,
+    affirmative_closing_statement: 6,
+    # negative_closing_statement_preparation: 11,
+    negative_closing_statement: 7,
+    waiting_end: 8,
+    finished: 9
   } do
   end
 
@@ -26,15 +28,17 @@ class Debate < ApplicationRecord
     waiting_start: "Finding participants...",
     count_in: "Count-down",
     affirmative_speech: "Affirmative opening arguments",
-    negative_cross_examination_preparation: "Negative questioning prep",
-    negative_cross_examination: "Negative questioning",
-    negative_speech_preparation: "Negative opening argument prep",
+    # negative_cross_examination_preparation: "Negative questioning prep",
+    # negative_cross_examination: "Negative questioning",
+    # negative_speech_preparation: "Negative opening argument prep",
     negative_speech: "Negative opening arguments",
-    affirmative_cross_examination_preparation: "Affirmative questioning prep",
     affirmative_cross_examination: "Affirmative questioning",
-    affirmative_closing_statement_preparation: "Affirmative closing prep",
+    negative_cross_examination: "Negative questioning",
+    # affirmative_cross_examination_preparation: "Affirmative questioning prep",
+    # affirmative_cross_examination: "Affirmative questioning",
+    # affirmative_closing_statement_preparation: "Affirmative closing prep",
     affirmative_closing_statement: "Affirmative closing",
-    negative_closing_statement_preparation: "Negative closing prep",
+    # negative_closing_statement_preparation: "Negative closing prep",
     negative_closing_statement: "Negative closing",
     waiting_end: "Final scoring",
     finished: "<h3>The winner is Someone</h3>"
@@ -42,18 +46,18 @@ class Debate < ApplicationRecord
 
   AFFIRMATIVE_PHASES = [
     :affirmative_speech,
-    :affirmative_cross_examination_preparation,
+    # :affirmative_cross_examination_preparation,
     :affirmative_cross_examination,
-    :affirmative_closing_statement_preparation,
+    # :affirmative_closing_statement_preparation,
     :affirmative_closing_statement
   ]
 
   NEGATIVE_PHASES = [
-    :negative_speech_preparation,
+    # :negative_speech_preparation,
     :negative_speech,
-    :negative_cross_examination_preparation,
+    # :negative_cross_examination_preparation,
     :negative_cross_examination,
-    :negative_closing_statement_preparation,
+    # :negative_closing_statement_preparation,
     :negative_closing_statement
   ]
 

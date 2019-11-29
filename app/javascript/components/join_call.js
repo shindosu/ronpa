8f35ba50e3f441632734bf54263ba3aaf11910f1
Noah Thorne <noahlthorne@gmail.com>
2019-11-29 15:00:10 +0900

    added back quit button and fixed phone thing

--- app/javascript/components/join_call.js
+++ app/javascript/components/join_call.js
@@ -1,15 +1,15 @@
-const removeCallButton = () => {
-  console.log("inside here");
+// const removeCallButton = () => {
+//   console.log("inside here");
 
-  const joinCall = document.getElementById("join-call")
-  if (joinCall) {
-    document.querySelector(".moderator-call").insertAdjacentHTML('beforeend', '<i class="fas fa-phone call-two"></i>');
-    document.querySelectorAll(".phone-call")[1].insertAdjacentHTML('beforeend', '<i class="fas fa-phone call"></i>');
-    joinCall.addEventListener("click", (event) => {
-      joinCall.remove();
-      document.querySelector(".phone-call").insertAdjacentHTML('beforeend', '<i class="fas fa-phone call-three"></i>');
-    })
-  }
-}
+//   const joinCall = document.getElementById("join-call")
+//   if (joinCall) {
+//     document.querySelector(".moderator-call").insertAdjacentHTML('beforeend', '<i class="fas fa-phone call-two"></i>');
+//     document.querySelectorAll(".phone-call")[1].insertAdjacentHTML('beforeend', '<i class="fas fa-phone call"></i>');
+//     joinCall.addEventListener("click", (event) => {
+//       joinCall.remove();
+//       document.querySelector(".phone-call").insertAdjacentHTML('beforeend', '<i class="fas fa-phone call-three"></i>');
+//     })
+//   }
+// }
 
-export { removeCallButton };
+// export { removeCallButton };
